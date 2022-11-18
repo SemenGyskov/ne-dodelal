4ce20e71654e980a5aad0de3cf37a9d8a63aa4b9
SemenGyskov <114874984+SemenGyskov@users.noreply.github.com>
2022-11-18 15:11:01 +0500

    Add files via upload

---
+++ ne-dodelal-main/подготовка в ЛР3/assests/js/main.js
@@ -0,0 +1,85 @@
+// 1
+function js_style(){
+document.getElementById("text").style.color = 'green'
+document.getElementById("text").style.fontSize = '20px'
+document.getElementById("text").style.width = '150px'
+document.getElementById("text").style.backgroundColor = 'black'
+document.getElementById("jsstyle").style.borderColor = 'red'
+}
+
+// 2
+
+function getFormvalue()
+{
+  var x = document.getElementById("form1")
+  for (var i = 0; i < x.length ; i++)
+  {
+   if (x.elements[i].value !='Submit')
+    {  
+      console.log(x.elements[i].value)
+     }  
+   }
+}
+
+// 3
+function getAttributes()
+{
+var x = document.getElementById("w3r").href
+alert('значение атребута href равно: '+x)
+var b = document.getElementById("w3r").hreflang
+alert('значение атребута hreflang равно: '+b)
+var c = document.getElementById("w3r").rel
+alert('значение атребута rel равно: '+c)
+var d = document.getElementById("w3r").target
+alert('значение атребута target равно: '+d)
+}
+
+// 4
+function insert_Row()
+{
+var x=document.getElementById('sampleTable').insertRow(0);
+var y = x.insertCell(0);
+var z = x.insertCell(1);
+y.innerHTML="новая строчка";
+z.innerHTML="еще новая строчка";
+document.getElementById('sampleTable').style.color = "red"
+document.getElementById('sampleTable').style.fontSize = '38px'
+}
+
+
+// 5
+
+function changeContent(){
+    row = window.prompt("Введите номер ряда","1");
+    col = window.prompt("Введите номер строчки","1");
+    content = window.prompt("Введите информацию");  
+    var x=document.getElementById('myTable').rows[parseInt(row,10)].cells;
+    x[parseInt(col,10)].innerHTML=content;
+}
+
+
+// 6
+
+
+// -
+
+
+
+// 7
+
+function getOptions()
+{
+var x=document.getElementById("mySelect");
+var txt1 = "Количество элементов: ";
+var i;
+l=document.getElementById("mySelect").length;  
+txt1 = txt1+l;
+for (i=0;i<x.length;i++)
+  {
+    txt1 = txt1 + "\n" + x.options[i].text;
+  }
+alert(txt1);
+}
+
+// 8
+
